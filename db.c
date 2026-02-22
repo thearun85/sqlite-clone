0bf30da4c3d65071b7e39360594d45df3776fad1
Arun Raghunath <arun2aruns@gmail.com>
2026-02-22 19:44:50 +0000

    feat: Meta command implemented

--- db.c
+++ db.c
@@ -23,6 +23,19 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);	
 }
 
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND,
+} MetaCommandResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+	if (strcmp(input_buffer->buffer, ".exit") == 0) {
+		close_input_buffer(input_buffer);
+		exit(EXIT_SUCCESS);
+	} else
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+}
+
 void read_input(InputBuffer* input_buffer) {
 	ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 	if (bytes_read <=0) {
@@ -44,12 +57,15 @@ int main(int argc, char *argv[])
 		print_prompt();
 		read_input(input_buffer);
 
-		if (strcmp(input_buffer->buffer, ".exit") == 0) {
-			close_input_buffer(input_buffer);
-			printf("[sqlite] Shutting down db");
-			exit(EXIT_SUCCESS);
-		} else {
-			printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+		if (input_buffer->buffer[0] == '.') {
+			switch (do_meta_command(input_buffer)) {
+				case (META_COMMAND_SUCCESS):
+					continue;
+				case (META_COMMAND_UNRECOGNIZED_COMMAND):
+					printf("[sqlite-db] Unrecognized command '%s.'\n", input_buffer->buffer);
+					continue;
+			}
 		}
+		
 	}
 }
