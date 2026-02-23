639fc74616778bd75178afcdd35ce520a868afa1
Arun Raghunath <arun2aruns@gmail.com>
2026-02-23 11:39:28 +0000

    feat: Insert row prepared with proper syntax

--- db.c
+++ db.c
@@ -38,6 +38,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 
 typedef enum {
 	PREPARE_SUCCESS,
+	PREPARE_SYNTAX_ERROR,
 	PREPARE_UNRECOGNIZED_STATEMENT,
 } PrepareResult;
 
@@ -47,14 +48,28 @@ typedef enum {
 	STATEMENT_DELETE,
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct {
+	uint32_t id;
+	char username[COLUMN_USERNAME_SIZE];
+	char email[COLUMN_EMAIL_SIZE];
+} Row;
+
 typedef struct {
 	StatementType type;
+	Row row_to_insert;
 } Statement;
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, 
 									Statement* statement) {
 	if (strncmp(input_buffer->buffer, "insert", 6) == 0){
 		statement->type = STATEMENT_INSERT;
+		int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), (statement->row_to_insert.username), (statement->row_to_insert.email));
+		if (args_assigned < 3) {
+			return PREPARE_SYNTAX_ERROR;
+		}
 		return PREPARE_SUCCESS;
 	} else if (strncmp(input_buffer->buffer, "select", 6) == 0) {
 		statement->type = STATEMENT_SELECT;
@@ -115,6 +130,8 @@ int main(int argc, char *argv[])
 		switch (prepare_statement(input_buffer, &statement)){
 			case PREPARE_SUCCESS:
 				break;
+			case PREPARE_SYNTAX_ERROR:
+				printf("[sqlite-db] Syntax error. Could not parse statement '%s'\n", input_buffer->buffer);
 			case PREPARE_UNRECOGNIZED_STATEMENT:
 				printf("[sqlite-db] Unrecognized command '%s'.\n", input_buffer->buffer);
 				continue;
